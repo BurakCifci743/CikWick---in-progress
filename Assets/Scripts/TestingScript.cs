c5739d98346098964a6e5b3a68aad00d6f5bdb17
Burak Çifçi <burakcifci93@gmail.com>
2025-06-20 11:24:06 +0300

    v0.1 Packages Imported

---
+++ Assets/Scripts/TestingScript.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class TestingScript : MonoBehaviour
+{
+    void Awake()
+    {
+        Debug.Log("awake");
+    }
+    void Start()
+    {
+        print("start");   
+    }
+}
