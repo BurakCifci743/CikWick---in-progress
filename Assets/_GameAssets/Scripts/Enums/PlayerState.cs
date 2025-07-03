0db173a58e6050047701758c55321c492f78ba0b
Burak Çifçi <burakcifci93@gmail.com>
2025-07-03 17:15:59 +0300

    v0.5 Player Controller Improvements

---
+++ Assets/_GameAssets/Scripts/Enums/PlayerState.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public enum PlayerState
+{
+    Idle,
+    Move,
+    Jump,
+    SlideIdle,
+    Slide
+}
+
+
+
--- Assets/_GameAssets/Scripts/Gameplay/Player/PlayerController.cs
+++ Assets/_GameAssets/Scripts/Gameplay/Player/PlayerController.cs
@@ -1,3 +1,4 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -12,7 +13,10 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private KeyCode _jumpKey;
     [SerializeField] private float _jumpForce;
     [SerializeField] private float _jumpCooldown;
-    [SerializeField] private bool canJump;
+    [SerializeField] private float _airMultiplier;
+    [SerializeField] private float _airDrag;
+
+    [SerializeField] private bool _canJump;
     [Header("Sliding Settings")]
     [SerializeField] private KeyCode _slideKey;
     [SerializeField] private float _slideMultiplier;
@@ -21,6 +25,9 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float _playerHeight;
     [SerializeField] private LayerMask _groundLayer;
     [SerializeField] private float _groundDrag;
+    
+
+    private StateController _stateController;
     private Rigidbody _playerRigidbody;
     private float _horizontalInput, _verticalInput;
     private Vector3 _movementDirection;
@@ -29,12 +36,14 @@ public class PlayerController : MonoBehaviour
 
     private void Awake()
     {
+        _stateController = GetComponent<StateController>();
         _playerRigidbody = GetComponent<Rigidbody>();
         _playerRigidbody.freezeRotation = true;
     }
     private void Update()
     {
         SetInputs();
+        SetStates();
         SetPlayerDrag();
         LimitPlayerSpeed();
     }
@@ -55,41 +64,66 @@ public class PlayerController : MonoBehaviour
         {
             _isSliding = false;
             Debug.Log("Player Moving");
-
         }
-
-        else if (Input.GetKey(_jumpKey) && canJump && IsGrounded()) // zıplama tuşuna basıldı mı ? - karakter zıplamaya uygun mu ? - şuanda yerde misin ? __ üçü birden true döndüğü durumda karakter zıplayacak.
+        // zıplama tuşuna basıldı mı ? - karakter zıplamaya uygun mu ? - şuanda yerde misin ? _-_ üçü birden true döndüğü durumda karakter zıplayacak.
+        else if (Input.GetKey(_jumpKey) && _canJump && IsGrounded()) 
         {
-            canJump = false;
+            _canJump = false;
             SetPlayerJumping();
             Invoke(nameof(ResetJump), _jumpCooldown);
         }
     }
+    private Vector3 GetMovementDirection()
+    {
+        return _movementDirection.normalized;
+    }
+
+
+    private void SetStates()
+    {
+        var movementDirection = GetMovementDirection();
+        var currentState = _stateController.GetCurrentState();
+
+        var newState = currentState switch
+        {
+            _ when !IsGrounded() => PlayerState.Jump,
+            _ when movementDirection == Vector3.zero && IsGrounded() && !isSliding() => PlayerState.Idle,
+            _ when movementDirection != Vector3.zero && IsGrounded() && !isSliding() => PlayerState.Move,
+            _ when movementDirection != Vector3.zero && IsGrounded() && isSliding() => PlayerState.Slide,
+            _ when movementDirection == Vector3.zero && IsGrounded() && isSliding() => PlayerState.SlideIdle,
+            _ => currentState
+        };
+
+        if (newState != currentState)
+        {
+            _stateController.ChangeState(newState);
+        }
+    }
+
     private void SetPlayerMovement()
     {
         _movementDirection = _orientationTransform.forward * _verticalInput + _orientationTransform.right * _horizontalInput;
-        if (_isSliding)
-        {
-            _playerRigidbody.AddForce(_movementDirection.normalized * _movementSpeed * _slideMultiplier, ForceMode.Force);
-        }
-        else
-        {
-            _playerRigidbody.AddForce(_movementDirection.normalized * _movementSpeed, ForceMode.Force);
 
-        }
+        float forceMultiplier = _stateController.GetCurrentState() switch
+        {
+            PlayerState.Move => 1f,
+            PlayerState.Slide => _slideMultiplier,
+            PlayerState.Jump => _airMultiplier,
+            _ => 1f
+        };
 
+        _playerRigidbody.AddForce(_movementDirection.normalized * _movementSpeed * forceMultiplier, ForceMode.Force);
+       
     }
     private void SetPlayerDrag()
     {
-        if (_isSliding)
+        _playerRigidbody.linearDamping = _stateController.GetCurrentState() switch
         {
-            _playerRigidbody.linearDamping = _slideDrag;
-        }
-        else
-        {
-            _playerRigidbody.linearDamping = _groundDrag;
-        }
-
+            PlayerState.Move => _groundDrag,
+            PlayerState.Slide => _slideDrag,
+            PlayerState.Jump => _airDrag,
+            _ => _playerRigidbody.linearDamping
+         };
     }
     private void LimitPlayerSpeed()
     {
@@ -97,7 +131,7 @@ public class PlayerController : MonoBehaviour
         if (flatVelocity.magnitude > _movementSpeed)
         {
             Vector3 limitedVelocity = flatVelocity.normalized * _movementSpeed;
-            _playerRigidbody.linearVelocity = new Vector3(limitedVelocity.x,limitedVelocity.y,limitedVelocity.z);
+            _playerRigidbody.linearVelocity = new Vector3(limitedVelocity.x, limitedVelocity.y, limitedVelocity.z);
         }
     }
 
@@ -111,11 +145,15 @@ public class PlayerController : MonoBehaviour
     }
     private void ResetJump()
     {
-        canJump = true;
+        _canJump = true;
     }
     private bool IsGrounded() // karakterin ayağından zemine doğru ışın gönderip , karakterin yerden mesafesini ölçeceğiz amaç isGrounded true döndürmek. false olduğu sürece zıplamasına izin vermeyeceğiz.
     {
         return Physics.Raycast(transform.position, Vector3.down, _playerHeight * 0.5f + 0.2f, _groundLayer); // player height işlemi bu oyun özelinde deneme-yanılma hesabı
+    }
+    private bool isSliding()
+    {
+        return _isSliding;
     }
 
 }
